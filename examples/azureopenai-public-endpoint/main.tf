c27bce10e5efb69168442aac853a3c2892b898c1
soferreira <soferreira@microsoft.com>
2023-05-08 10:37:06 +0200

    add public endpoint example

---
+++ examples/azureopenai-public-endpoint/main.tf
@@ -0,0 +1,17 @@
+module "openai" {
+  source              = "../.."
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  deployment = {
+    "text-curie-001" = {
+      name          = "text-davinci-003"
+      model_format  = "OpenAI"
+      model_name    = "text-davinci-003"
+      model_version = "1"
+      scale_type    = "Standard"
+    },
+  }
+  depends_on = [
+    azurerm_resource_group.this
+  ]
+}
