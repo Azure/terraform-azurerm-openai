23321353eef0e9bcf849998dc31b9bb0ccbc33d3
soferreira <soferreira@microsoft.com>
2023-05-12 15:51:02 +0200

    add missing attrs to azurerm_cognitive_account

--- main.tf
+++ main.tf
@@ -13,13 +13,54 @@ locals {
 }
 
 resource "azurerm_cognitive_account" "this" {
-  kind                          = "OpenAI"
-  location                      = local.location
-  name                          = local.azureopenai_account_name
-  resource_group_name           = data.azurerm_resource_group.this.name
-  sku_name                      = var.sku_name
-  custom_subdomain_name         = local.azureopenai_customsubdomain_name
-  public_network_access_enabled = var.public_network_access_enabled
+  kind                               = "OpenAI"
+  location                           = local.location
+  name                               = local.azureopenai_account_name
+  resource_group_name                = data.azurerm_resource_group.this.name
+  sku_name                           = var.sku_name
+  custom_subdomain_name              = local.azureopenai_customsubdomain_name
+  public_network_access_enabled      = var.public_network_access_enabled
+  dynamic_throttling_enabled         = var.dynamic_throttling_enabled
+  fqdns                              = var.fqdns
+  local_auth_enabled                 = var.local_auth_enabled
+  outbound_network_access_restricted = var.outbound_network_access_restricted
+
+  dynamic "customer_managed_key" {
+    for_each = var.customer_managed_key != null ? [var.customer_managed_key] : []
+    content {
+      key_vault_key_id   = customer_managed_key.value.key_vault_key_id
+      identity_client_id = customer_managed_key.value.identity_client_id
+    }
+  }
+
+  dynamic "identity" {
+    for_each = var.identity != null ? [var.identity] : []
+    content {
+      type         = identity.value.identity_type
+      identity_ids = identity.value.identity_ids
+    }
+  }
+
+  dynamic "network_acls" {
+    for_each = var.network_acls != null ? [var.network_acls] : []
+    content {
+      default_action = network_acls.value.network_acls_default_action
+      ip_rules       = network_acls.value.ip_rules
+      virtual_network_rules {
+        subnet_id                            = network_acls.value.virtual_network_rules.subnet_id
+        ignore_missing_vnet_service_endpoint = network_acls.value.virtual_network_rules.ignore_missing_vnet_service_endpoint
+      }
+    }
+  }
+
+  dynamic "storage" {
+    for_each = var.storage != null ? [var.storage] : []
+    content {
+      storage_account_id = storage.value.storage_account_id
+      identity_client_id = storage.value.identity_client_id
+    }
+  }
+  
   tags = merge(local.tags, (/*<box>*/ (var.tracing_tags_enabled ? { for k, v in /*</box>*/ {
     avm_git_commit           = "c8b6b17b0b28a2aa54a3e734b9bd0a0d0ef5c267"
     avm_git_file             = "main.tf"
