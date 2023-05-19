0f4ab7f7144efaef16afbc1b0e3d4b9e6ca0a29d
Sofia Ferreira <101214403+soferreira@users.noreply.github.com>
2023-05-19 11:48:38 +0200

    add missing attrs to azurerm_cognitive_account (#7)
    * add missing attrs to azurerm_cognitive_account

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "random_integer" "this" {
 }
 
 locals {
-  azureopenai_account_name         = coalesce(var.azureopenai_account_name, "azureopenai-${random_integer.this.result}")
-  azureopenai_customsubdomain_name = coalesce(var.azureopenai_customsubdomain_name, "azureopenai-${random_integer.this.result}")
+  account_name          = coalesce(var.account_name, "azure-openai-${random_integer.this.result}")
+  custom_subdomain_name = coalesce(var.custom_subdomain_name, "azure-openai-${random_integer.this.result}")
   tags = var.default_tags_enabled ? {
     Application_Name = var.application_name
     Environment      = var.environment
@@ -13,13 +13,57 @@ locals {
 }
 
 resource "azurerm_cognitive_account" "this" {
-  kind                          = "OpenAI"
-  location                      = local.location
-  name                          = local.azureopenai_account_name
-  resource_group_name           = data.azurerm_resource_group.this.name
-  sku_name                      = var.sku_name
-  custom_subdomain_name         = local.azureopenai_customsubdomain_name
-  public_network_access_enabled = var.public_network_access_enabled
+  kind                               = "OpenAI"
+  location                           = local.location
+  name                               = local.account_name
+  resource_group_name                = data.azurerm_resource_group.this.name
+  sku_name                           = var.sku_name
+  custom_subdomain_name              = local.custom_subdomain_name
+  public_network_access_enabled      = var.public_network_access_enabled
+  dynamic_throttling_enabled         = var.dynamic_throttling_enabled
+  fqdns                              = var.fqdns
+  local_auth_enabled                 = var.local_auth_enabled
+  outbound_network_access_restricted = var.outbound_network_access_restricted
+
+  dynamic "customer_managed_key" {
+    for_each = var.customer_managed_key != null ? [var.customer_managed_key] : []
+    content {
+      key_vault_key_id   = customer_managed_key.value.key_vault_key_id
+      identity_client_id = customer_managed_key.value.identity_client_id
+    }
+  }
+
+  dynamic "identity" {
+    for_each = var.identity != null ? [var.identity] : []
+    content {
+      type         = identity.value.identity_type
+      identity_ids = identity.value.identity_ids
+    }
+  }
+
+  dynamic "network_acls" {
+    for_each = var.network_acls != null ? [var.network_acls] : []
+    content {
+      default_action = network_acls.value.default_action
+      ip_rules       = network_acls.value.ip_rules
+      dynamic "virtual_network_rules" {
+        for_each = network_acls.value.virtual_network_rules != null ? network_acls.value.virtual_network_rules : []
+        content {
+          subnet_id                            = virtual_network_rules.value.subnet_id
+          ignore_missing_vnet_service_endpoint = virtual_network_rules.value.ignore_missing_vnet_service_endpoint
+        }
+      }
+    }
+  }
+
+  dynamic "storage" {
+    for_each = var.storage
+    content {
+      storage_account_id = storage.value.storage_account_id
+      identity_client_id = storage.value.identity_client_id
+    }
+  }
+
   tags = merge(local.tags, (/*<box>*/ (var.tracing_tags_enabled ? { for k, v in /*</box>*/ {
     avm_git_commit           = "c8b6b17b0b28a2aa54a3e734b9bd0a0d0ef5c267"
     avm_git_file             = "main.tf"
--- variables.tf
+++ variables.tf
@@ -14,13 +14,13 @@ variable "application_name" {
   description = "Name of the application. A corresponding tag would be created on the created resources if `var.default_tags_enabled` is `true`."
 }
 
-variable "azureopenai_account_name" {
+variable "account_name" {
   type        = string
   default     = ""
   description = "Specifies the name of the Cognitive Service Account. Changing this forces a new resource to be created. Leave this variable as default would use a default name with random suffix."
 }
 
-variable "azureopenai_customsubdomain_name" {
+variable "custom_subdomain_name" {
   type        = string
   default     = ""
   description = "The subdomain name used for token-based authentication. Changing this forces a new resource to be created. Leave this variable as default would use a default name with random suffix."
@@ -61,17 +61,85 @@ variable "deployment" {
   nullable    = false
 }
 
+variable "dynamic_throttling_enabled" {
+  type        = bool
+  default     = null
+  description = "Determines whether or not dynamic throttling is enabled. If set to `true`, dynamic throttling will be enabled. If set to `false`, dynamic throttling will not be enabled."
+}
+
 variable "environment" {
   type        = string
   default     = ""
   description = "Environment of the application. A corresponding tag would be created on the created resources if `var.default_tags_enabled` is `true`."
 }
 
-# tflint-ignore: terraform_unused_declarations
-variable "network_acls_default_action" {
-  type        = string
-  default     = "Deny"
-  description = "Azure OpenAI account network ACLs default action."
+variable "customer_managed_key" {
+  type = object({
+    key_vault_key_id   = string
+    identity_client_id = optional(string)
+  })
+  default     = null
+  description = <<-DESCRIPTION
+    type = object({
+      key_vault_key_id   = (Required) The ID of the Key Vault Key which should be used to Encrypt the data in this OpenAI Account.
+      identity_client_id = (Optional) The Client ID of the User Assigned Identity that has access to the key. This property only needs to be specified when there're multiple identities attached to the OpenAI Account.
+    })
+  DESCRIPTION
+}
+
+variable "fqdns" {
+  type        = list(string)
+  default     = null
+  description = "List of FQDNs allowed for the Cognitive Account."
+}
+
+
+variable "identity" {
+  type = object({
+    type         = string
+    identity_ids = optional(list(string))
+  })
+  default     = null
+  description = <<-DESCRIPTION
+    type = object({
+      type         = (Required) The type of the Identity. Possible values are `SystemAssigned`, `UserAssigned`, `SystemAssigned, UserAssigned`.
+      identity_ids = (Optional) Specifies a list of User Assigned Managed Identity IDs to be assigned to this OpenAI Account.
+    })
+  DESCRIPTION
+}
+
+variable "local_auth_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether local authentication methods is enabled for the Cognitive Account. Defaults to `true`."
+}
+
+variable "network_acls" {
+  type = set(object({
+    default_action = string
+    ip_rules       = optional(set(string))
+    virtual_network_rules = optional(set(object({
+      subnet_id                            = string
+      ignore_missing_vnet_service_endpoint = optional(bool, false)
+    })))
+  }))
+  default     = null
+  description = <<-DESCRIPTION
+    type = set(object({
+      default_action = (Required) The Default Action to use when no rules match from ip_rules / virtual_network_rules. Possible values are `Allow` and `Deny`.
+      ip_rules                    = (Optional) One or more IP Addresses, or CIDR Blocks which should be able to access the Cognitive Account.
+      virtual_network_rules = optional(set(object({
+        subnet_id                            = (Required) The ID of a Subnet which should be able to access the OpenAI Account.
+        ignore_missing_vnet_service_endpoint = (Optional) Whether ignore missing vnet service endpoint or not. Default to `false`.
+      })))
+    }))
+  DESCRIPTION
+}
+
+variable "outbound_network_access_restricted" {
+  type        = bool
+  default     = false
+  description = "Whether outbound network access is restricted for the Cognitive Account. Defaults to `false`."
 }
 
 variable "pe_subresource" {
@@ -89,7 +157,7 @@ variable "private_dns_zone" {
   description = <<-DESCRIPTION
   A map of object that represents the existing Private DNS Zone you'd like to use. Leave this variable as default would create a new Private DNS Zone.
   type = object({
-    name = "(Required) The name of the Private DNS Zone."
+    name                = "(Required) The name of the Private DNS Zone."
     resource_group_name = "(Optional) The Name of the Resource Group where the Private DNS Zone exists. If the Name of the Resource Group is not provided, the first Private DNS Zone from the list of Private DNS Zones in your subscription that matches `name` will be returned."
   }
 DESCRIPTION
@@ -110,14 +178,14 @@ variable "private_endpoint" {
   description = <<-DESCRIPTION
   A map of objects that represent the configuration for a private endpoint."
   type = map(object({
-    name = (Required) Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created.
-    vnet_rg_name = (Required) Specifies the name of the Resource Group where the Private Endpoint's Virtual Network Subnet exists. Changing this forces a new resource to be created.
-    vnet_name = (Required) Specifies the name of the Virtual Network where the Private Endpoint's Subnet exists. Changing this forces a new resource to be created.
-    subnet_name = (Required) Specifies the name of the Subnet which Private IP Addresses will be allocated for this Private Endpoint. Changing this forces a new resource to be created.
+    name                               = (Required) Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created.
+    vnet_rg_name                       = (Required) Specifies the name of the Resource Group where the Private Endpoint's Virtual Network Subnet exists. Changing this forces a new resource to be created.
+    vnet_name                          = (Required) Specifies the name of the Virtual Network where the Private Endpoint's Subnet exists. Changing this forces a new resource to be created.
+    subnet_name                        = (Required) Specifies the name of the Subnet which Private IP Addresses will be allocated for this Private Endpoint. Changing this forces a new resource to be created.
     dns_zone_virtual_network_link_name = (Optional) The name of the Private DNS Zone Virtual Network Link. Changing this forces a new resource to be created. Default to `dns_zone_link`.
-    private_dns_entry_enabled = (Optional) Whether or not to create a `private_dns_zone_group` block for the Private Endpoint. Default to `false`.
-    private_service_connection_name = (Optional) Specifies the Name of the Private Service Connection. Changing this forces a new resource to be created. Default to `privateserviceconnection`.
-    is_manual_connection = (Optional) Does the Private Endpoint require Manual Approval from the remote resource owner? Changing this forces a new resource to be created. Default to `false`.
+    private_dns_entry_enabled          = (Optional) Whether or not to create a `private_dns_zone_group` block for the Private Endpoint. Default to `false`.
+    private_service_connection_name    = (Optional) Specifies the Name of the Private Service Connection. Changing this forces a new resource to be created. Default to `privateserviceconnection`.
+    is_manual_connection               = (Optional) Does the Private Endpoint require Manual Approval from the remote resource owner? Changing this forces a new resource to be created. Default to `false`.
   }))
 DESCRIPTION
   nullable    = false
@@ -127,6 +195,21 @@ variable "public_network_access_enabled" {
   type        = bool
   default     = false
   description = "Whether public network access is allowed for the Cognitive Account. Defaults to `false`."
+}
+
+variable "storage" {
+  type = list(object({
+    storage_account_id = string
+    identity_client_id = optional(string)
+  }))
+  default     = []
+  description = <<-DESCRIPTION
+    type = list(object({
+      storage_account_id = (Required) Full resource id of a Microsoft.Storage resource.
+      identity_client_id = (Optional) The client ID of the managed identity associated with the storage resource.
+    }))
+  DESCRIPTION
+  nullable    = false
 }
 
 variable "sku_name" {
